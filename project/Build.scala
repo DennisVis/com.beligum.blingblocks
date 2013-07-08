25922bbbff10cd68a3082d90687b3032938bc771
Bram Biesbrouck <b@beligum.com>
2013-07-08 19:54:37 +0200

    Enter version 0.1.0

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import play.Project._
 object ApplicationBuild extends Build {
 
   val appName         = "com_beligum_blingblocks"
-  val appVersion      = "1.0-SNAPSHOT"
+  val appVersion      = "0.1.0"
 
   val appDependencies = Seq(
   	javaCore,
